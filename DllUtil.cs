feaed33cc6a9b653246c7850f9d850ee611846f3
King John <znybn1@gmail.com>
2018-03-13 15:54:13 +0800

    Add files via upload

---
+++ DllUtil.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class DllUtil : EditorWindow
+{
+
+    Vector2 scrollPos;
+
+    Object dll;
+    string dllPath;
+    Dictionary<Object,Object> dllScripts = new Dictionary<Object,Object>();
+
+
+    struct IDTRans
+    {
+        public string keyID;
+        public string valueID;
+        public string keyGuid;
+        public string valueGuid;
+    }
+
+    List<IDTRans> ids = new List<IDTRans>();
+    List<bool> replaceDone = new List<bool>();
+    object l = new object();
+
+
+    bool autoRefresh = false;
+
+     
+    [MenuItem("Window/DllUtil")]
+	static void Init()
+    {
+        GetWindow<DllUtil>().Show();
+    }
+
+    void Update()
+    {
+//        lock (replaceDone)
+        {
+            int n = replaceDone.Count;
+
+            if (n != 0)
+            {
+                int done = replaceDone.Count(b => b == true);
+                //Debug.Log(done);
+                if (done != n)
+                {
+                    EditorUtility.DisplayCancelableProgressBar("DllReplace", string.Format("{0}/{1}", done, n), (float)done / (float)n);
+                }
+                else
+                {
+                    EditorPrefs.SetBool("kAutoRefresh", autoRefresh);
+                    EditorUtility.ClearProgressBar();
+                    replaceDone.Clear();
+                    AssetDatabase.Refresh();
+                }
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.BeginVertical(GUILayout.Width(250f));
+
+        Object obj = EditorGUILayout.ObjectField(dll, typeof(UnityEngine.Object), false);
+        if(obj != dll)
+        {
+            if(obj == null)
+            {
+                dll = null;
+                dllPath = "";
+                return;
+            }
+            Debug.Log(obj.GetType());
+            dllPath = AssetDatabase.GetAssetPath(obj);
+            if(!dllPath.EndsWith(".dll"))
+            {
+                ShowNotification(new GUIContent("Not a dll"));
+                dllPath = AssetDatabase.GetAssetPath(dll);
+                return;
+            } 
+            dllScripts.Clear();
+            dll = AssetDatabase.LoadMainAssetAtPath(dllPath);
+            GetAllDllMonoScripts(dllPath);
+            GetSameNativeMonoScripts();
+        }
+        if(dll == null)
+        {
+            return;
+        }
+
+        if (GUILayout.Button("Refresh"))
+        {
+            GetAllDllMonoScripts(dllPath);
+            GetSameNativeMonoScripts();
+        }
+
+        if(dllScripts != null && dllScripts.Count > 0)
+        {
+            if (EditorSettings.serializationMode == SerializationMode.ForceText)
+            {
+                if (GUILayout.Button("Replace dll Scripts with Native"))
+                {
+                    ReplaceDllScriptWithNative();
+                }
+                if (GUILayout.Button("Replace Native Scripts with dll"))
+                {
+                    ReplaceNativeScriptWithDll();
+                }
+                
+            }
+            else
+            {
+                GUILayout.Label("使用该工具前必须将Unity默认序列化模式设置为强制为文本");
+                if (GUILayout.Button("Set Asset Serialization to Force Text"))
+                {
+                    EditorSettings.serializationMode = SerializationMode.ForceText;
+                }
+            }
+        }
+
+        EditorGUILayout.EndVertical();
+
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+
+
+        foreach(var script in dllScripts)
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUILayout.ObjectField(script.Key,typeof(Object),false,GUILayout.Width(200f));
+            if(GUILayout.Button("ref",GUILayout.Width(30f)))
+            {
+                EditorUtil.SelectReferencesInProject(Unsupported.GetLocalIdentifierInFile(script.Key.GetInstanceID()).ToString());
+            }
+            if(script.Key != script.Value)
+            {
+                EditorGUILayout.LabelField("<==>", GUILayout.Width(50f));
+                EditorGUILayout.ObjectField(script.Value, typeof(Object), false, GUILayout.Width(200f));
+                if (GUILayout.Button("ref", GUILayout.Width(30f)))
+                {
+                    EditorUtil.SelectReferencesInProject(AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(script.Value)));
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        EditorGUILayout.EndScrollView();
+
+        EditorGUILayout.EndHorizontal();
+    }
+
+    void GetAllDllMonoScripts(string dllPath)
+    {
+        dllScripts = AssetDatabase.LoadAllAssetsAtPath(dllPath).ToDictionary(a => a);
+    }
+
+    void GetSameNativeMonoScripts()
+    {
+        Dictionary<Object, Object> tmp = new Dictionary<Object, Object>();
+        foreach (var script in dllScripts)
+        {
+            string[] guids = AssetDatabase.FindAssets(script.Key.name + " t:Script");
+            foreach (string guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+
+                if (path.EndsWith("/" + script.Key.name + ".cs", true, System.Globalization.CultureInfo.CurrentCulture))
+                {
+                    tmp.Add(script.Key, AssetDatabase.LoadMainAssetAtPath(path));
+                    break;
+                }
+            }
+        }
+        foreach (var t in tmp)
+        {
+            dllScripts[t.Key] = t.Value;
+        }
+    }
+
+    void ReplaceDllScriptWithNative()
+    {
+        ids.Clear();
+
+        foreach (var s in dllScripts)
+        {
+            if (s.Key == s.Value)
+            {
+                continue;
+            }
+            ids.Add(new IDTRans
+            {
+                valueID = Unsupported.GetLocalIdentifierInFile(s.Key.GetInstanceID()).ToString(),
+                valueGuid = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(s.Key)),
+                keyID = Unsupported.GetLocalIdentifierInFile(s.Value.GetInstanceID()).ToString(),
+                keyGuid = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(s.Value))
+            });
+//            Debug.Log("keyGUID:" + ids[ids.Count - 1].keyGuid + "\tvaluGUID:" + ids[ids.Count - 1].valueGuid);
+        }
+        autoRefresh = EditorPrefs.GetBool("kAutoRefresh");
+        EditorPrefs.SetBool("kAutoRefresh", false);
+        EditorUtil.FindReferencesInProject(new string[]{AssetDatabase.AssetPathToGUID(dllPath)}, paths =>
+        {
+            replaceDone.Clear();
+            replaceDone = paths.Select(s => false).ToList();
+            Debug.Log(replaceDone.Count);
+
+            foreach (var path in paths)
+            {
+                ThreadPool.QueueUserWorkItem(ReplaceProcess, path);
+            }
+        });
+    }
+
+
+    void ReplaceNativeScriptWithDll()
+    {
+        ids.Clear();
+        
+        foreach(var s in dllScripts)
+        {
+            if(s.Key == s.Value)
+            {
+                continue;
+            }
+            ids.Add(new IDTRans
+            {
+                keyID = Unsupported.GetLocalIdentifierInFile(s.Key.GetInstanceID()).ToString(),
+                keyGuid = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(s.Key)),
+                valueID = Unsupported.GetLocalIdentifierInFile(s.Value.GetInstanceID()).ToString(),
+                valueGuid = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(s.Value))
+            });
+            
+        }
+        autoRefresh = EditorPrefs.GetBool("kAutoRefresh");
+        EditorPrefs.SetBool("kAutoRefresh", false);
+        EditorUtil.FindReferencesInProject(dllScripts.Select(s => AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(s.Value))).ToArray(), paths =>
+        {
+            replaceDone.Clear();
+            replaceDone = paths.Select(s => false).ToList();
+            Debug.Log(replaceDone.Count);
+
+            foreach (var path in paths)
+            {
+                ThreadPool.QueueUserWorkItem(ReplaceProcess, path);
+            }
+        });
+    }
+
+    void ReplaceProcess(object obj)
+    {
+        
+        string path = obj.ToString();
+        string[] lines = File.ReadAllLines(path);
+        for (int i = 0; i < lines.Length; i++)
+        {
+            for(int j = 0;j<ids.Count;j++)
+            {
+                if (lines[i].Contains(ids[j].valueGuid) && lines[i].Contains(ids[j].valueID))
+                {
+                    lines[i] = lines[i].Replace(ids[j].valueID, ids[j].keyID);
+                    lines[i] = lines[i].Replace(ids[j].valueGuid, ids[j].keyGuid);
+                }
+            }
+        }
+        File.WriteAllLines(path, lines);
+        lock (replaceDone)
+        {
+            for(int i = 0;i<replaceDone.Count;i++)
+            {
+                if(replaceDone[i] == false)
+                {
+                    replaceDone[i] = true;
+                    break;
+                }
+            }
+        }
+        //Debug.Log(path);
+    }
+
+    void OnSelectionChange()
+    {
+        Debug.Log(Unsupported.GetLocalIdentifierInFile(Selection.activeInstanceID));
+    }
+
+}
---
+++ EditorUtil.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class EditorUtil
+{
+
+    [MenuItem("Assets/Find References In Project",true)]
+    public static bool IsSelectionFile()
+    {
+        return Selection.activeObject != null;
+    }
+
+    #region Reference
+
+    public const string searchfilter = "t:AnimatorController t:Prefab t:Scene t:ScriptableObject";
+    public const string animationSearchFilter = "t:AnimationClip";
+    public const string materialSearchFilter = "t:Material";
+
+    [MenuItem("Assets/Find References In Project")]
+    public static void SelectReferencesInProject()
+    {
+        SelectReferencesInProject(Selection.assetGUIDs);
+    }
+
+    public static void SelectReferencesInProject(string assetGUID)
+    {
+        SelectReferencesInProject(new string[] { assetGUID });
+    }
+
+    public static void SelectReferencesInProject(string[] assetGUIDs)
+    {
+        FindReferencesInProject(assetGUIDs, r =>
+        {
+            EditorApplication.ExecuteMenuItem("Window/Project");
+            if (r.Count > 0)
+            {
+                FoldProject();
+                Debug.Log("引用数：" + r.Count);
+            }
+            else
+            {
+                Debug.Log("没有引用");
+            }
+            List<UnityEngine.Object> passInObjs = new List<UnityEngine.Object>();
+            foreach(string guid in assetGUIDs)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                if(!string.IsNullOrEmpty(path))
+                {
+                    passInObjs.Add(AssetDatabase.LoadMainAssetAtPath(path));
+                }
+            }
+            Selection.objects = r.Select(s => AssetDatabase.LoadMainAssetAtPath(s)).Concat(passInObjs).ToArray();
+            onFindFinished = null;
+            r.Clear();
+            //EditorUtility.UnloadUnusedAssetsImmediate(false);
+        });
+    }
+
+
+    public class References
+    {
+        public string path;
+        public string[] assetGUIDs;
+    }
+
+    static int assetNum = 0;
+    static volatile int dealNum = 0;
+    static volatile List<string> references = new List<string>();
+    static Action<List<string>> onFindFinished;
+    static object lockObj = new object();
+
+    static List<string> allUnityAssetPaths = new List<string>();
+    static List<string> dealtPaths = new List<string>();
+
+    public static List<string> GetAllUnityAssets()
+    {
+        string[] anims = Directory.GetFiles(Application.dataPath, "*.anim", SearchOption.AllDirectories);
+        for (int i = 0; i < anims.Length;i++ )
+        {
+            anims[i] = anims[i].Replace(Application.dataPath, "Assets");
+        }
+        string[] materials = AssetDatabase.FindAssets(materialSearchFilter);
+        materials = materials.Select(s => AssetDatabase.GUIDToAssetPath(s)).Where(s=>string.IsNullOrEmpty(Path.GetExtension(s))).ToArray();
+        List<string> paths = AssetDatabase.FindAssets(searchfilter).Select(s => AssetDatabase.GUIDToAssetPath(s)).ToList();
+        paths.AddRange(anims);
+        return paths;
+    }
+
+    public static void FindReferencesInProject(string assetGUID, Action<List<string>> onFinished)
+    {
+        FindReferencesInProject(new string[] { assetGUID }, onFindFinished);
+    }
+
+    public static void FindReferencesInProject(string[] assetGUIDs,Action<List<string>> onFinished)
+    {
+        if(EditorSettings.serializationMode != SerializationMode.ForceText)
+        {
+            if(EditorUtility.DisplayDialog("", "如果你要使用该功能，必须设置Unity的默认序列化模式为ForceText", "Set SerializationMode to ForceText", "Cancel"))
+            {
+                EditorSettings.serializationMode = SerializationMode.ForceText;
+            }
+            return;
+        }
+
+        references.Clear();
+        onFindFinished = onFinished;
+        dealtPaths.Clear();
+        allUnityAssetPaths = GetAllUnityAssets();
+        assetNum = allUnityAssetPaths.Count;
+        dealNum = 0;
+        EditorApplication.update += Updata;
+
+        foreach (string path in allUnityAssetPaths)
+        {
+            References r = new References
+            {
+                assetGUIDs = assetGUIDs,
+                path = path,
+            };
+            ThreadPool.QueueUserWorkItem(FindReferences, r);
+        }
+    }
+
+    static void FindReferences(object obj)
+    {
+        References r = obj as References;
+        string file = File.ReadAllText(r.path);
+        foreach (string guid in r.assetGUIDs)
+        {
+            //Regex.IsMatch(file, guid);
+            //if (Regex.IsMatch(file, guid))
+            if (file.Contains(guid))
+            {
+                lock (lockObj)
+                {
+                    if (!references.Contains(r.path))
+                    {
+                        references.Add(r.path);
+                    }
+                }
+                break;
+            }
+        }
+        file = null;
+        lock(lockObj)
+        {
+            dealNum++;
+            dealtPaths.Add(r.path);
+        }
+        
+    }
+
+    public static void Updata()
+    {
+        if (assetNum > 0)
+        {
+            if (assetNum != dealNum)
+            {
+                if(EditorUtility.DisplayCancelableProgressBar("Find References", string.Format("{0}/{1}", dealNum, assetNum), (float)dealNum / (float)assetNum))
+                {
+                    EditorUtility.ClearProgressBar();
+                    assetNum = 0;
+                    dealNum = 0;
+                    EditorApplication.update -= Updata;
+                    foreach(string p in allUnityAssetPaths)
+                    {
+                        if(!dealtPaths.Contains(p))
+                        {
+                            Debug.Log(p);
+                        }
+                    }
+                    dealtPaths.Clear();
+                    allUnityAssetPaths.Clear();
+                }
+            }
+            else
+            {
+                EditorUtility.ClearProgressBar();
+                assetNum = 0;
+                dealNum = 0;
+                if(onFindFinished != null)
+                {
+                    onFindFinished(references);
+                }
+                EditorApplication.update -= Updata;
+            }
+        }
+        else
+        {
+            EditorUtility.ClearProgressBar();
+            EditorApplication.update -= Updata;
+        }
+    }
+
+    #endregion
+
+    [MenuItem("Assets/Fold Project", true, 0)]
+    public static bool IsOneColumn()
+    {
+        Type pwu = typeof(ProjectWindowUtil);
+        MethodInfo getPBMeth = pwu.GetMethod("GetProjectBrowserIfExists", BindingFlags.NonPublic | BindingFlags.Static);
+        object pb = getPBMeth.Invoke(null, null);
+        FieldInfo treeViewInfo = pb.GetType().GetField("m_AssetTree", BindingFlags.Instance | BindingFlags.NonPublic);
+        object treeView = treeViewInfo.GetValue(pb);
+        return treeView == null ? false : true;
+    }
+
+    /// <summary>
+    /// 收起整个项目视图，只在Project视图为单列模式时可用
+    /// </summary>
+    [MenuItem("Assets/Fold Project",false,0)]
+    public static void FoldProject()
+    {
+        //EditorGUIUtility.PingObject(AssetDatabase.LoadMainAssetAtPath("Assets"));
+        //Debug.Log(EditorPrefs.GetString("AndroidSdkRoot"));
+
+        //foreach(int i in InternalEditorUtility.expandedProjectWindowItems)
+        //{
+        //    Debug.Log(i);
+        //}
+        
+        Type pwu = typeof(ProjectWindowUtil);
+        MethodInfo getPBMeth = pwu.GetMethod("GetProjectBrowserIfExists", BindingFlags.NonPublic | BindingFlags.Static);
+        object pb = getPBMeth.Invoke(null,null);
+        FieldInfo treeViewInfo = pb.GetType().GetField("m_AssetTree", BindingFlags.Instance | BindingFlags.NonPublic);
+        object treeView = treeViewInfo.GetValue(pb);
+        if(treeView == null)
+        {
+            Debug.LogWarning("收起整个项目视图功能只在Project视图为单列模式时可用");
+            return;
+        }
+        PropertyInfo dataInfo = treeView.GetType().GetProperty("data");
+        object data = dataInfo.GetValue(treeView,null);
+        FieldInfo rootItemInfo = data.GetType().GetField("m_RootItem", BindingFlags.NonPublic | BindingFlags.Instance);
+        object rootItem = rootItemInfo.GetValue(data);
+        Debug.Log(rootItem);
+        MethodInfo expendInfo = data.GetType().GetMethod("SetExpandedWithChildren");
+        expendInfo.Invoke(data, new object[] { rootItem, false });
+    }
+}
